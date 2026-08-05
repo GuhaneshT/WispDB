ac8adbf447cece38d0213581ef122f3825312bcd
GuhaneshT <guhanesh2222@gmail.com>
2026-08-05 23:01:28 +0530

    refactor : cont

--- main/wisp.go
+++ main/wisp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"wisp/memtable"
+	"wisp/sstable"
 	"wisp/wal"
 )
 
@@ -13,12 +14,11 @@ type Record struct {
 }
 
 type Wisp struct {
-	wal               *wal.WAL
-	mutableMemTable   *memtable.MemTable
+	wal *wal.WAL
+	mutableMemTable *memtable.MemTable
 	immutableMemTable *memtable.MemTable
-	sstable           *SSTable // Placeholder for future SSTable implementation
-	// tbd
-	// sstable
+	sstablewriter *sstable.Writer
+	
 }
 
 func CreateWisp() (*Wisp, error) {
@@ -26,48 +26,92 @@ func CreateWisp() (*Wisp, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	mutableMemTable, err := memtable.CreateMemTable()
 	if err != nil {
 		return nil, err
 	}
-	return &Wisp{wal: walInstance, mutableMemTable: mutableMemTable, immutableMemTable: nil}, nil
+
+	return &Wisp{
+		wal:               walInstance,
+		mutableMemTable:   mutableMemTable,
+		immutableMemTable: nil,
+	}, nil
 }
 
-func (w *Wisp) Insert(seriesId uint64, timestamp int64, value []byte) error {
+func (w *Wisp) Insert(
+	seriesId uint64,
+	timestamp int64,
+	value []byte,
+) error {
+
 	record := wal.WALRecord{
 		Timestamp: timestamp,
 		SeriesID:  seriesId,
 		Value:     value,
 	}
+
 	err := w.wal.AppendRecord(record)
+
 	if err != nil {
 		return err
 	}
+
 	if w.mutableMemTable.IsFull() {
+
 		w.mutableMemTable.Freeze()
 		w.immutableMemTable = w.mutableMemTable
 		w.mutableMemTable, _ = memtable.CreateMemTable()
 	}
+
 	status := w.mutableMemTable.Put(seriesId, record.Timestamp, value)
-	if !status {
+	if !status{
 		return fmt.Errorf("failed to put record in mutable memtable")
 	}
+
 	return nil
 }
 
-// recover on startup, to be added after sstable is implemented
+
+//recover on startup, to be added after sstable is implemented
 func (w *Wisp) Recover() error {
 	records, err := w.wal.Replay()
 	if err != nil {
 		return err
 	}
+
 	for _, record := range records {
-		ok := w.mutableMemTable.Put(record.SeriesID, record.Timestamp, record.Value)
+		ok := w.mutableMemTable.Put(
+			record.SeriesID,
+			record.Timestamp,
+			record.Value,
+		)
+
 		if !ok {
-			return fmt.Errorf("failed to recover record: seriesID=%d timestamp=%d", record.SeriesID, record.Timestamp)
+			return fmt.Errorf(
+				"failed to recover record: seriesID=%d timestamp=%d",
+				record.SeriesID,
+				record.Timestamp,
+			)
 		}
 	}
+
 	return nil
 }
 
-func (w *Wisp) Freeze() error { return nil }
+func (w *Wisp) Flush() error{
+
+	it := w.immutableMemTable.Iterator()
+
+	for it.Valid() {
+		key, value := it.Entry()
+		// write to SSTable
+		fmt.Printf("Flushing to SSTable: SeriesID=%d Timestamp=%d Value=%s\n", key.SeriesID, key.Timestamp, string(value))
+		it.Next()
+	}
+
+	
+	w.immutableMemTable = nil
+
+	return nil
+}
--- memtable/memtable.go
+++ memtable/memtable.go
@@ -5,9 +5,9 @@ import (
 )
 
 type MemTable struct {
-	skiplist       *skiplist.Skiplist
-	size           uint64
-	mutable        bool
+	skiplist *skiplist.Skiplist
+	size	 uint64
+	mutable bool
 	flushThreshold uint64
 }
 
@@ -15,17 +15,40 @@ type MemTableIterator struct {
 	iterator *skiplist.Iterator
 }
 
-func CreateMemTable() (*MemTable, error) {
-	return &MemTable{skiplist: skiplist.NewSkipList(16), mutable: true, flushThreshold: 4 * 1024 * 1024}, nil
+func (m *MemTable) Iterator() *MemTableIterator {
+    return &MemTableIterator{
+        iterator: m.skiplist.Iterator(),
+    }
 }
 
-func (m *MemTable) Put(seriesID uint64, timestamp int64, value []byte) bool {
+func (it *MemTableIterator) Valid() bool {
+    return it.iterator.Valid()
+}
+
+func (it *MemTableIterator) Next() {
+    it.iterator.Next()
+}
+
+func (it *MemTableIterator) Entry() (skiplist.Key, []byte) {
+    return it.iterator.Entry()
+}
+
+func CreateMemTable() (*MemTable, error) {
+	return &MemTable{
+		skiplist:       skiplist.NewSkipList(16),
+		mutable:        true,
+		flushThreshold: 4 * 1024 * 1024, // 4 MB for now
+	}, nil
+}
+
+func (m *MemTable) Put(seriesID uint64,timestamp int64, value []byte) (bool) {
 	if !m.mutable {
 		return false
 	}
 	m.skiplist.Insert(skiplist.Key{SeriesID: seriesID, Timestamp: timestamp}, value)
 	m.size += 8 + 8 + uint64(len(value))
 	return true
+
 }
 
 func (m *MemTable) Get(seriesID uint64, timestamp int64) ([]byte, bool) {
@@ -40,8 +63,14 @@ func (m *MemTable) Size() uint64 {
 	return m.size
 }
 
-func (m *MemTable) IsFull() bool { return m.size >= m.flushThreshold }
+func (m *MemTable) IsFull() bool {
 
-func (m *MemTable) Freeze() {
+//tbd
+return m.size >= m.flushThreshold
+}
+
+func (m *MemTable) Freeze(){
 	m.mutable = false
 }
+
+
